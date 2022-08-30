38c1ba179efcd0eb32601af9b0181da68daba0aa
ezravkatz <ezravkatz@gmail.com>
2022-08-30 00:09:06 -0400

    added db info to table

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,36 @@
+const { redirect } = require("express/lib/response");
+const mysql = require("mysql");
+
+//connection
+const pool = mysql.createPool({
+  connectionLimit: 100,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
 
 //View users
 exports.view((req, res) => {
-  res.render('main');
+  
+
+pool.getConnection((err, connection) => {
+  if (err) throw err; //not connected
+  console.log("Connected as ID" + connection.threadId);
+});
+
+//user connection
+connection.query('SELECT * FROM user', (err, rows) => {
+  //when finished, release
+  connection.release();
+
+  if(!err) {
+    redirect.render('main', { rows });
+  } else {
+    console.log(err);
+  }
+
+  console.log('Data from user table: \n', rows);
+});
+
 });
